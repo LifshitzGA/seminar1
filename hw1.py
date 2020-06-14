430dabf23bbf6314e34a9ca535c59641eb55b635
LifshitzGA <glifshitz13@gmail.com>
2020-06-14 20:11:46 +0300

    hw1.py

---
+++ hw1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 25 11:27:32 2020
+
+@author: georgij
+"""
+
+import pandas as pd
+import numpy as np
+
+pd.read_csv ('pokemon.cvs')
+
+data = pd.read_csv('/Users/georgij/Desktop/xxx/pokemon.csv')
+
+data
