b76582a3a0d8e13808a69f1988b1a0ca558ba7cb
LifshitzGA <glifshitz13@gmail.com>
2020-06-28 13:49:09 +0300

    hw2.py

---
+++ hw2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 12:18:19 2020
+
+@author: georgij
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+eq = pd.read_csv("Earthquake-database.csv")
+
+d = pd.Index.value_counts(eq["INTENSITY"])
+c = pd.Index.value_counts(eq["YEAR"])
+
+print (c)
+
+
+fig, axes = plt.subplots()
+plt.bar(d.index, d)
+plt.ylabel('Количество землятресений')
+plt.xlabel('Сила землетрясений')
+
+fig, axes = plt.subplots()
+plt.bar(c.index, c)
+plt.ylabel('Количество землятресений')
+plt.xlabel('Год')
